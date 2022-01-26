76fe533247665af9f09a3c9b06f490ae79526672
Rayhan-abdullha <captainrayhan215@gmail.com>
2022-01-26 09:02:11 -0800

    some changess

--- index.js
+++ index.js
@@ -9,24 +9,15 @@ const categoryRoute = require("./routes/categories");
 const multer = require("multer")
 const path = require("path")
 
-const port = 5000
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use("/images", express.static(path.join(__dirname,"/images")))
 
 dotenv.config()
 
-mongoose.connect(MONGO_URL).then(console.log("Connected to Mongodb"),{ 
-    useNewUrlParser: true,
-    useCreateIndex: true
-  })
+mongoose.connect(process.env.MONGO_URL).then(console.log("Connected to Mongodb"))
 .catch(err => console.log(err))
-
-
-// test
-app.get("/", (req, res) => {
-    res.send("Hello world");
-})
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -42,10 +33,11 @@ app.post("/api/upload", upload.single("file"), (req,res) => {
     res.status(200).json("file has been uploded")
 })
 
-
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
 app.use("/api/posts", postsRoute)
 app.use("/api/categories", categoryRoute)
 
-app.listen(process.env.PORT || port)
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
