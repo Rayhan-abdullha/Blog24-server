7714db21d7c4606cae90a79029aa2dff3bc6e2f2
Rayhan-abdullha <captainrayhan215@gmail.com>
2022-01-26 08:36:51 -0800

    some mistack changes

--- index.js
+++ index.js
@@ -16,7 +16,10 @@ app.use("/images", express.static(path.join(__dirname,"/images")))
 
 dotenv.config()
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2nody.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(console.log("Connected to Mongodb"))
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2nody.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(console.log("Connected to Mongodb"),{ 
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
 .catch(err => console.log(err))
 
 // test
