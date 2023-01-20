0dd60c72a975dc199cbb0116d0ab3aa9a82ac3dc
Rayhan-abdullha <captainrayhan215@.com>
2023-01-20 09:09:02 +0600

    final

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/user")
+const postsRoute = require("./routes/post")
+const categoryRoute = require("./routes/categories");
+const multer = require("multer")
+const path = require("path")
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json())
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+dotenv.config()
+app.use("/images", express.static(path.join(__dirname,"/images")))
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2nody.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`).then(console.log("Connected to Mongodb"))
+.catch(err => console.log(err))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images")
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name)
+    }
+})
+
+app.get("/", (req, res) => {
+    res.send("Server Starting...")
+})
+
+const upload = multer({storage: storage})
+app.post("/api/upload", upload.single("file"), (req,res) => {
+    res.status(200).json("file has been uploded")
+})
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute)
+app.use("/api/posts", postsRoute)
+app.use("/api/categories", categoryRoute)
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+});
+
+module.exports = app
---
+++ routes/post.js
@@ -0,0 +1,98 @@
+const router = require("express").Router();
+const User = require("../models/User")
+const Post = require("../models/Post")
+
+// Create Post
+router.post("/", async(req, res) => {
+  const newPost = new Post(req.body)
+  try {
+      const savedPost = await newPost.save()
+      res.status(200).json(savedPost)
+  } catch(err) {
+      res.status(500).json(err)
+  }
+  
+})
+
+
+// Update
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userName === req.body.userName) {
+      try {
+        const updatedPost = await Post.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedPost);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can update only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Delete
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userName === req.body.userName) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(401).json("You can delete only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//GET POST
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//GET ALL POSTS
+router.get("/", async (req, res) => {
+const userName = req.query.user;
+const catName = req.query.cat;
+
+try {
+  let posts;
+  if (userName) {
+    posts = await Post.find({ userName });
+
+  } else if (catName) {
+    posts = await Post.find({
+      categories: {
+        $in: [catName],
+      },
+    });
+  } else {
+    posts = await Post.find();
+  }
+  res.status(200).json(posts);
+} catch (err) {
+  res.status(500).json(err);
+}
+
+});
+module.exports = router 
